a787799359aca4e7a07ca021536c3a16db678445
guisheng.li <gary.li.rocks@gmail.com>
2017-06-28 14:47:44 +1200

    add another test

--- tests/gradeTests.js
+++ tests/gradeTests.js
@@ -21,8 +21,20 @@ exports["Can add new grade"] = function(test) {
 exports["Can average grades"] = function(test) {
     book.addGrade(100);
     book.addGrade(50);
-
     var average = book.getAverage();
     test.equal(average, 75);
+    test.done();
+};
+
+exports["Can get letter grade"] = function(test) {
+    book.addGrade(100);
+    book.addGrade(90);
+    var letterGrade = book.getLetterGrade();
+    test.equal(letterGrade, 'A');
+
+    book.reset();
+    book.addGrade(20);
+    var letterGrade = book.getLetterGrade();
+    test.equal(letterGrade, 'F');
     test.done();
 }
